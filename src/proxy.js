0abe6e16561385779f98f0291f8306995d1a8f66
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-16 14:58:31 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,5 +1,6 @@
-const request = require('request');
-const pick = require('lodash').pick;
+const fetch = require('node-fetch');
+// TODO: drop lodash.pick in favor of a native destructure.
+const pick = require('lodash.pick');
 const shouldCompress = require('./shouldCompress');
 const redirect = require('./redirect');
 const compress = require('./compress');
@@ -7,39 +8,33 @@ const bypass = require('./bypass');
 const copyHeaders = require('./copyHeaders');
 
 function proxy(req, res) {
-  request.get(
-    req.params.url,
-    {
-      headers: {
-        ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-        'user-agent': 'Bandwidth-Hero Compressor',
-        'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        via: '1.1 bandwidth-hero'
-      },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) {
-        return redirect(req, res);
-      }
-
-      copyHeaders(origin, res);
-      res.setHeader('content-encoding', 'identity');
-      req.params.originType = origin.headers['content-type'] || '';
-      req.params.originSize = buffer.length;
-
-      if (shouldCompress(req)) {
-        compress(req, res, buffer);
-      } else {
-        bypass(req, res, buffer);
-      }
-    }
-  );
+    fetch(
+        req.params.url,
+        {
+            headers: {
+                ...pick(req.headers, ['cookie', 'dnt', 'referer']),
+                'user-agent': 'Bandwidth-Hero Compressor',
+                'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
+                via: '1.1 bandwidth-hero'
+            },
+        })
+        .then(origin => {
+            if (!origin.ok) {
+                return redirect(req, res);
+            }
+            req.params.originType = origin.headers.get('content-type') || '';
+            origin.buffer().then(buffer => {
+                req.params.originSize = buffer.length;
+                copyHeaders(origin, res);
+                res.setHeader('content-encoding', 'identity');
+                if (shouldCompress(req)) {
+                    compress(req, res, buffer)
+                } else {
+                    bypass(req, res, buffer)
+                }
+            })
+        })
+        .catch(e => console.log(e));
 }
 
 module.exports = proxy;
