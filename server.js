0482a67df7ba4bb758ac97b0ed98cf82d2d57d74
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-18 11:14:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = require('express')();
 const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const proxy = require('./src/proxy');
-const compression = require('compression');
+//const compression = require('compression');
 
-app.use(compression())
+//app.use(compression())
 
 app.enable('trust proxy');
 app.get('/', authenticate, params, proxy);
