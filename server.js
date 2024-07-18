a1b962495b8521cbb5e74d2f8f2ba0338eb3a158
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-18 11:17:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 //const compression = require('compression');
+const PORT = process.env.PORT || 8080;
 
 //app.use(compression())
 
-app.enable('trust proxy');
+app.disable('trust proxy');
 app.get('/', authenticate, params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(8080);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));;
