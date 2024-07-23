28dae9dbfcc2b2289350b18ea385ac700c3c3eb5
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-23 13:01:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 const PORT = process.env.PORT || 8080;
-
+const hostname = "0.0.0.0";
 
 
 
 app.get('/', authenticate, params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));;
+app.listen(PORT, hostname, () => console.log(`Listening on ${hostname}:${PORT}/`));;
